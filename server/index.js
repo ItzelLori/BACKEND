cbcdee1c2ded3d2b4ea664e8ba820bcf57c339de
AlfredoDompablo <57891371+AlfredoDompablo@users.noreply.github.com>
2024-07-01 14:40:25 -0600

    update cors

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import morgan from 'morgan';
 import {PORT} from './config.js';
 import indexRoutes from './routes/index.routes.js'
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 app.use(morgan('dev'));
+app.use(cors());
 app.use('/api', indexRoutes);
 
 
